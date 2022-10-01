50dcfcca989f10061e643f06a89999678e53d5de
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-10-01 10:12:20 +0800

    fix(build): external vite

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,4 @@
-import { defineBuildConfig } from 'unbuild'
+import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: ['./src/index.ts'],
@@ -7,4 +7,5 @@ export default defineBuildConfig({
   rollup: {
     emitCJS: true,
   },
-})
+  externals: ['vite'],
+});
