f4e55e11e4ac501d7526395ec54c77ae66c4ab38
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-10-04 11:24:11 +0800

    test: utils unit test

---
+++ test/constant.ts
@@ -0,0 +1,48 @@
+import type { Options } from '../src'
+
+export const ogOptions: Options = {
+  basic: {
+    url: 'https://lmmmmmm.me',
+    title: '_lmmmmmm',
+    type: 'image.png',
+    image: 'https://lmmmmmm.me/avatar.png',
+  },
+  extra: {
+    determiner: 'auto',
+    description: '_lmmmmmm, Front-end Developer.',
+    locale: 'zh_CN',
+    localeAlternate: ['fr_FR', 'es_ES'],
+    siteName: '_lmmmmmm',
+    audio: {
+      url: 'audio url',
+      secureUrl: 'audio secure url',
+      type: 'video.movie',
+    },
+    video: 'video meta',
+  },
+  twitter: {
+    image: 'https://lmmmmmm.me/avatar.png',
+    imageAlt: 'twitter image alt',
+    player: 'player',
+    playerWidth: 1200,
+    playerHeight: 600,
+    playerStream: 'player stream',
+    app: {
+      name: {
+        iphone: 'iphone name',
+        ipad: 'ipad name',
+        googleplay: 'google play name',
+      },
+      id: {
+        iphone: 'iphone url',
+        ipad: 'ipad url',
+        googleplay: 'google play url',
+      },
+      url: {
+        iphone: 'iphone url',
+        ipad: 'ipad url',
+        googleplay: 'google play url',
+      },
+    },
+  },
+}
---
+++ test/utils.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest'
+import { camelcase, toAttrs } from '../src/utils'
+import { ogOptions } from './constant'
+
+describe('camelcase function', () => {
+  it('lowercase name', () => {
+    expect(camelcase('hi')).toEqual('hi')
+    expect(camelcase('name')).toEqual('name')
+  })
+
+  it('normal name', () => {
+    expect(camelcase('helloWorld')).toEqual('hello:world')
+    expect(camelcase('localeAlternate')).toEqual('locale:alternate')
+    expect(camelcase('playerWidth')).toEqual('player:width')
+  })
+
+  it('exception field name', () => {
+    expect(camelcase('secureUrl')).toEqual('secure_url')
+    expect(camelcase('siteName')).toEqual('site_name')
+  })
+
+  it('empty string', () => {
+    expect(camelcase('')).toEqual('')
+  })
+})
+
+describe('toAttrs function', () => {
+  it('snapshot', () => {
+    const basicOGMetaAttrs = Object.entries(ogOptions.basic).map(([name, content]) => toAttrs(camelcase(name), content, 'og'))
+    const extraOGMetaAttrs = Object.entries(ogOptions.extra ?? {}).map(([name, content]) => toAttrs(camelcase(name), content, 'og'))
+    const twitterOGMetaAttrs = Object.entries(ogOptions.twitter ?? {}).map(([name, content]) => toAttrs(camelcase(name), content, 'twitter'))
+
+    expect(basicOGMetaAttrs).toMatchSnapshot()
+    expect(extraOGMetaAttrs).toMatchSnapshot()
+    expect(twitterOGMetaAttrs).toMatchSnapshot()
+  })
+})
