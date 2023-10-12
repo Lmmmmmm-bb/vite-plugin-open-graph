2f31d2b7ac868f8e9aaf9844f9daa2a612e3c83e
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-10-12 15:32:45 +0800

    chore: clean up

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,12 @@ import antfu from '@antfu/eslint-config';
 
 export default antfu({}, {
   rules: {
+    'antfu/top-level-function': 'off',
+
+    'curly': ['error', 'all'],
+
     'style/semi': ['error', 'always'],
     'style/member-delimiter-style': ['error', {}],
-    'antfu/top-level-function': 'off',
-    'curly': ['error', 'all'],
     'style/brace-style': ['error', '1tbs', { allowSingleLine: false }],
-    'style/member-delimiter-style': ['error', {}],
   },
 });
