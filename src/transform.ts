67fc8c279931e7749e98b8666cd1126855f9b8a5
_lmmmmmm <lmmmmmm12138@gmail.com>
2024-10-15 01:21:00 +0800

    fix: facebook meta key

--- src/transform.ts
+++ src/transform.ts
@@ -6,7 +6,7 @@ import { camelcase, toAttrs } from './utils.ts';
 export const transform = (options: Options): HtmlTagDescriptor[] => {
   const basicOGMetaAttrs = Object.entries(options.basic || {}).map(([name, content]) => toAttrs(camelcase(name), content, 'property', 'og'));
   const twitterOGMetaAttrs = Object.entries(options.twitter || {}).map(([name, content]) => toAttrs(camelcase(name), content, 'name', 'twitter'));
-  const facebookOGMetaAttrs = Object.entries(options.facebook || {}).map(([name, content]) => toAttrs(camelcase(name), content, 'name', 'fb'));
+  const facebookOGMetaAttrs = Object.entries(options.facebook || {}).map(([name, content]) => toAttrs(camelcase(name), content, 'property', 'fb'));
 
   const attrs = [...basicOGMetaAttrs.flat(), ...twitterOGMetaAttrs.flat(3), ...facebookOGMetaAttrs.flat()];
 
