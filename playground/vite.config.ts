0e1cf93398d52fe28f5b761af5e78e8fd1fa4e3b
_lmmmmmm <lmmmmmm12138@gmail.com>
2025-01-07 11:46:22 +0800

    chore: lint fix

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,6 @@
+import type { Options } from 'vite-plugin-open-graph';
 import { defineConfig } from 'vite';
 import ogPlugin from 'vite-plugin-open-graph';
-import type { Options } from 'vite-plugin-open-graph';
 
 const ogOptions: Options = {
   basic: {
